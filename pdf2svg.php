588f1547b4109f11bef3306d14fd7307ff82f839
Rainer Rillke <rillke@wikipedia.de>
2014-07-16 16:24:31 +0200

    Create pdf2svg.php

---
+++ pdf2svg.php
@@ -0,0 +1,62 @@
+<?php
+$tool_user_name = 'convert';
+
+include_once ( 'shared/common.php' ) ;
+
+$host = getProtocol() . '://tools.wmflabs.org/convert/';
+
+if (!array_key_exists('file', $_FILES)) {
+  header("Location: $host");
+  die();
+}
+$uploadName = $_FILES['file']['tmp_name'];
+$fileName = $uploadName . '.pdf';
+$targetName = $fileName . '.svg';
+
+if ($_FILES['file']['size'] > 1000000) {
+  unlink($uploadName);
+  header("Location: $host#tooBig");
+  die();
+}
+
+if (!move_uploaded_file($uploadName, $fileName)) {
+  echo('cant move uploaded file');
+  unlink($uploadName);
+  header("Location: $host#cantmove");
+  die();
+}
+
+exec('pdf2svg ' . escapeshellarg($fileName) . ' ' . escapeshellarg($targetName));
+unlink($fileName);
+
+$handle = fopen($targetName, 'r');
+
+if ($handle === false) {
+  echo('error converting the file');
+  header("Location: $host#conversionError");
+  die();
+}
+
+if (filesize($targetName) > 5000000) {
+  fclose($handle);
+  unlink($targetName);
+  echo('output unexpectedly huge');
+  header("Location: $host#outputTooHuge");
+  die();
+}
+
+$content = file_get_contents($targetName);
+fclose($handle);
+unlink($targetName);
+
+if (strlen($content) > 5000000) {
+  echo('output unexpectedly huge 2');
+  header("Location: $host#outputTooHuge2");
+  die();
+}
+
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+header('Content-type: image/svg+xml');
+header('Content-Disposition: attachment; filename="' . addslashes($_FILES['file']['name']) . '.svg"');
+echo $content;
+die();
